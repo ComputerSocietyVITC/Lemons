4bc8762c481626f18836571b9470278006a62e9c
TheChilledBuffalo <abhishekgs.tvm@gmail.com>
2024-12-30 17:34:53 +0900

    feat: added endpoint for teams

---
+++ src/routes/team/index.ts
@@ -0,0 +1,124 @@
+import { OpenAPIHono } from "@hono/zod-openapi";
+
+import prisma from "../../lib/prisma-client.js";
+import { getTeam, getTeamById, createTeam, deleteTeam } from "./routes.js";
+
+import { Role } from "@prisma/client";
+import { checkRole, getCurrentUser } from "../../lib/auth-provider.js";
+
+const teamRouter = new OpenAPIHono();
+
+teamRouter.openapi(createTeam, async (ctx) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id: getCurrentUser(ctx).id },
+    });
+    const allowedRoles: Role[] = ["SUPER_ADMIN", "ADMIN"];
+    const { name, imageId } = ctx.req.valid("json");
+
+    if (!checkRole(allowedRoles, ctx) && !user?.isLeader) {
+      return ctx.text("Unauthorized", 403);
+    }
+
+    await prisma.team.create({
+      data: {
+        name: name,
+        imageId: imageId,
+      },
+    });
+
+    return ctx.text("Team created successfully.", 200);
+  } catch (error) {
+    console.error("Error occurred", error);
+    return ctx.text("An unexpected error occurred", 500);
+  }
+});
+
+teamRouter.openapi(getTeam, async (ctx) => {
+  try {
+    const allowedRoles: Role[] = ["SUPER_ADMIN", "ADMIN", "EVALUATOR"];
+
+    if (!checkRole(allowedRoles, ctx)) {
+      return ctx.text("Unauthorized", 403);
+    }
+
+    const teams = await prisma.team.findMany();
+
+    if (!teams) {
+      return ctx.text("No team have registered", 404);
+    }
+
+    return ctx.json({ teams }, 200);
+  } catch (error) {
+    console.error("Error occurred", error);
+    return ctx.text("An unexpected error occurred", 500);
+  }
+});
+
+teamRouter.openapi(getTeamById, async (ctx) => {
+  try {
+    const teamId = ctx.req.param("id");
+    const user = getCurrentUser(ctx);
+
+    if (user.role === "USER") {
+      const fetchedUser = await prisma.user.findUnique({
+        where: { id: user.id },
+        include: { team: true },
+      });
+
+      if (!fetchedUser?.team) {
+        return ctx.text("You are not part of any team", 404);
+      }
+
+      if (teamId !== fetchedUser.team.id) {
+        return ctx.text("You do not have permission to view other team", 403);
+      }
+
+      const team = await prisma.team.findUnique({
+        where: { id: teamId },
+      });
+
+      return ctx.json(team, 200);
+    }
+
+    const team = await prisma.team.findUnique({
+      where: { id: teamId },
+    });
+
+    if (!team) {
+      return ctx.text("Team not found", 404);
+    }
+
+    return ctx.json({ team }, 200);
+  } catch (error) {
+    console.error("Error occurred", error);
+    return ctx.text("An unexpected error occurred", 500);
+  }
+});
+
+teamRouter.openapi(deleteTeam, async (ctx) => {
+  try {
+    const allowedRoles: Role[] = ["SUPER_ADMIN"];
+
+    if (!checkRole(allowedRoles, ctx)) {
+      return ctx.text("Unauthorized", 403);
+    }
+
+    const id = ctx.req.param("id");
+
+    if ((await prisma.team.findUnique({ where: { id } })) == null) {
+      return ctx.text("Team not found", 404);
+    }
+
+    await prisma.team.delete({
+      where: { id },
+    });
+
+    return ctx.text("Team deleted successfully.", 200);
+  } catch (error) {
+    console.error("Error occurred", error);
+    return ctx.text("An unexpected error occurred", 500);
+  }
+});
+
+export default teamRouter;
---
+++ src/routes/team/routes.ts
@@ -0,0 +1,113 @@
+import { z, createRoute } from "@hono/zod-openapi";
+import { TeamSchema } from "../../schemas/team";
+
+export const getTeam = createRoute({
+  method: "get",
+  path: "/",
+  responses: {
+    200: {
+      description: "All team retrieved successfully",
+      content: {
+        "application/json": {
+          schema: z.array(
+            z.object({
+              team: TeamSchema,
+            })
+          ),
+        },
+      },
+    },
+    403: {
+      description: "Unauthorized - User does not have permission",
+    },
+    404: {
+      description: "No team have registered",
+    },
+    500: {
+      description: "Server error",
+    },
+  },
+});
+
+export const getTeamById = createRoute({
+  method: "get",
+  path: "/{id}",
+  request: {
+    params: z.object({
+      id: z.string().uuid(),
+    }),
+  },
+  responses: {
+    200: {
+      description: "Team retrieved successfully",
+      content: {
+        "application/json": {
+          schema: z.object({
+            team: TeamSchema,
+          }),
+        },
+      },
+    },
+    403: {
+      description: "Unauthorized - User does not have permission",
+    },
+    404: {
+      description: "Team not found",
+    },
+    500: {
+      description: "Server error",
+    },
+  },
+});
+
+export const createTeam = createRoute({
+  method: "post",
+  path: "/",
+  request: {
+    body: {
+      content: {
+        "application/json": {
+          schema: z.object({
+            name: z.string(),
+            imageId: z.string().uuid(),
+          }),
+        },
+      },
+    },
+  },
+  responses: {
+    200: {
+      description: "Team created successfully",
+    },
+    403: {
+      description: "Unauthorized - User does not have permission",
+    },
+    500: {
+      description: "Server error",
+    },
+  },
+});
+
+export const deleteTeam = createRoute({
+  method: "delete",
+  path: "/",
+  request: {
+    params: z.object({
+      name: z.string(),
+    }),
+  },
+  responses: {
+    200: {
+      description: "Team deleted successfully",
+    },
+    403: {
+      description: "Unauthorized - User does not have permission",
+    },
+    404: {
+      description: "Team not found",
+    },
+    500: {
+      description: "Server error",
+    },
+  },
+});
